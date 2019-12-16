acd4f7d67ea709fe337a9b88dcede86d4a8b7291
Brucks <Brucks@Brucks-PC>
2019-12-16 13:33:18 +0100

    Maven build and dependencies defined

---
+++ db-services2/src/db/connection/PSQLConnectorService.java
@@ -0,0 +1,32 @@
+package db.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class PSQLConnectorService {
+
+	public Connection createConnection() {
+
+		String user = "michl";
+		String password = "michl";
+		try {
+			String url = "jdbc:postgresql://localhost:5432/betting_db?currentSchema=public";
+			Connection connection = DriverManager.getConnection(url, user, password);
+			System.out.println("Connected to the PostgreSQL server successfully.");
+			return connection;
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return null;
+	}
+
+	public static void main(String[] args) {
+		PSQLConnectorService psqlConnectorService = new PSQLConnectorService();
+		psqlConnectorService.createConnection();
+	}
+
+}
