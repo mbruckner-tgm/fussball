a436e6b1dc6cc935693099e05d71827f32f0e8b8
mbruckner-tgm <mbruckner1996@gmail.com>
2019-12-23 20:09:40 +0100

    some changes so far ... many to go ...

--- db-services/src/db/entity/Player.java
+++ db-services/src/db/entity/Player.java
@@ -1,4 +1,4 @@
-package db.data;
+package db.entity;
 
 public class Player {
 
--- db-services/src/db/service/PlayersRepository.java
+++ db-services/src/db/service/PlayersRepository.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import db.connection.PSQLConnectorService;
-import db.data.Player;
+import db.entity.Player;
 
 public class PlayersRepository {
 	
